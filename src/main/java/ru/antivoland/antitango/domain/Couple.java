37a54012110ec53ac3edb0f2c64b5652649592dd
antivoland <antivoland@inbox.ru>
2015-04-18 04:14:20 +0300

    couple position

--- src/main/java/ru/antivoland/antitango/domain/Couple.java
+++ src/main/java/ru/antivoland/antitango/domain/Couple.java
@@ -4,9 +4,15 @@ package ru.antivoland.antitango.domain;
 // todo: leader's twist angle
 // todo: distance from start point along the circle of dance
 public class Couple {
+    public static class Position {
+        Double distance; // radial coordinate
+        Double angle; // angular coordinate
+    }
+
     Style style;
     Leader leader;
     Follower follower;
     Embrace embrace;
     Lift lift;
+    Position position;
 }
